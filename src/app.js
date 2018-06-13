b67e6a48d7c0a419ac058be43bc6a80f6c839380
James Dube <thelonesoldier2301@gmail.com>
2018-06-13 15:21:45 +0200

    get the full story for each blog post

--- src/app.js
+++ src/app.js
@@ -3,5 +3,7 @@ var _ = require('lodash');
 
 //get all posts
 scrapper.getPosts().then((posts) =>{
-
+    scrapper.bindPosts(posts).then((posts) => {
+		console.log('app finished scrapping: ',posts.length,'posts')
+    })
 })
--- src/scrapper.js
+++ src/scrapper.js
@@ -24,5 +24,53 @@ module.exports = {
 			.then(posts => success(posts))
 			.catch(e => error(e))
 		}) 
-    }
+    },
+    getPostBody(Post) {
+		let tempPost = Post
+		console.log('preparing to get post body');
+		return new Promise((success, error) => {
+			const nightmare = Nightmare({ show: false })
+			nightmare
+	  		.goto(tempPost.link).wait(9000).evaluate(() => {
+				  	return $('#comp-ivpe94mh_SinglePostMediaTop_MediaPost__0_0__type_MediaPost').text()
+			}).end()
+			.then((body) => {
+				console.log('extracting body for : ',tempPost.title);
+				tempPost.body = body
+				success(tempPost)
+			})
+			.catch((e) => error(e))
+		}).catch('error getting post body');
+	},
+	/**
+     * Bind the body of a post
+     * 
+     * @param {Object} Posts  
+     */
+	bindPosts(Posts) {
+		var originalPosts = Posts;
+		return new Promise((resolve) => {
+			let Promises = [];
+			for (i = 0; i < Posts.length; i++) {
+				Promises.push(
+					this.getPostBody(Posts[i]).then((post) => {
+						var foundPost = _.find(Posts, [ 'title', post.title ]);
+						
+						if(typeof (foundPost) != undefined && foundPost.title != undefined){
+							console.log('Found post : ', foundPost.title);
+							//remove the post from the array and add the updated one
+							var interimPosts = _.reject(originalPosts, [ 'title', foundPost.title ]);
+							interimPosts.push(foundPost);
+							originalPosts = interimPosts;
+						}else{
+							console.log('could not find post')
+						}
+					})
+				);
+			}
+			Promise.all(Promises).then(() => {
+				resolve(originalPosts);
+			});
+		});
+	}
 }
